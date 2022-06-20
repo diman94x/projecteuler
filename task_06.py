62b4c21eb4cd82d6d92724c21a7992205e29287a
JustDoTheCode <26868689+JustDoTheCode@users.noreply.github.com>
2022-06-20 14:00:48 +0700

    Решение задач

--- task_06.py
+++ task_06.py
@@ -1,6 +1,7 @@
 def task_6():
     """Разность между суммой квадратов и квадратом суммы"""
-    return sum([i for i in range(1, 101)]) ** 2 - sum([i*i for i in range(1, 101)])
+    return sum((i for i in range(1, 101))) ** 2 - sum((i * i for i in range(1, 101)))
+
 
 if __name__ == '__main__':
     print(task_6())
